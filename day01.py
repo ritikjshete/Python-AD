d542ecaa1defd5a63c65e8b1cf430db47b11703f
ritikjshete <ritikjshete07@gmail.com>
2025-07-25 23:27:20 +0530

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,60 @@
+#1.	Write a Python program to take two numbers as input from the user and display their sum.
+
+num1 =  int(input("Enter the first no:"))
+num2 =  int(input("Enter the second no:"))
+
+sum = num1+num2
+print(f'Sum = {sum}')
+
+#2.	Calculate and print the midpoint of two numbers entered by the user.
+
+num1 =  int(input("Enter the first no:"))
+num2 =  int(input("Enter the second no:"))
+
+mid = (num1+num2)/2
+print(f'mid = {mid}')
+
+#3.	Write a Python program to calculate the area of a triangle using the formula: area = 0.5 * base * height.
+height = int(input("Enter the height:"))
+base = int(input("Enter the base:"))
+area = 0.5 *base* height
+
+print(f'Area of triangle = {area}')
+
+#4.	Take three numbers as input from the user and display their product.
+num1 =  int(input("Enter the first no:"))
+num2 =  int(input("Enter the second no:"))
+num3 =  int(input("Enter the therd no:"))
+
+product = num1*num2*num3
+
+print(f'product = {product}')
+
+#5.	Take a number as input and display its square and cube.
+num1 =  int(input("Enter the no:"))
+
+print(f'Sqare = {num1**2} and cube = {num1**3}' )
+
+#6.	Take two numbers as input and display their quotient and remainder.
+num1 =  int(input("Enter the first no:"))
+num2 =  int(input("Enter the second no:"))
+
+remainder = num1%num2
+quotient = num1/num2
+
+#7.	Take three numbers as input and display their average.
+num1 =  int(input("Enter the first no:"))
+num2 =  int(input("Enter the second no:"))
+num3 =  int(input("Enter the third no:"))
+
+Avg = (num1+num2+num3)/3
+print(f'Avg = {Avg}')
+
+#8.	Write a Python program to calculate the area of a rectangle using the formula: area = length * width.
+height = int(input("Enter the height:"))
+Width = int(input("Enter the width:"))
+area = height * Width
+
+print(f'Area of rectangle = {area}')
+
+#9.	Take five numbers as input representing marks obtained and total marks.
